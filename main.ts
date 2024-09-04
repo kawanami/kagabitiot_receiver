5185d5a3b2d6e8239059e17bc1bdc8d54a6c6802
Takashi Kawanami <takashi.kawanami@gmail.com>
2024-09-04 12:44:24 +0900

    起動時にWiFiリンク対応

--- main.ts
+++ main.ts
@@ -4,8 +4,8 @@ input.onButtonPressed(Button.A, function () {
     basic.showArrow(ArrowNames.South)
     basic.pause(1000)
 })
-// 2回すると上手くいく。謎。
 input.onButtonPressed(Button.AB, function () {
+    // 無線接続 2回送信すると確実
     basic.showIcon(IconNames.Heart)
     serial.writeLine("ss=" + ssid)
     basic.pause(1000)
@@ -39,7 +39,7 @@ input.onButtonPressed(Button.B, function () {
 let pass = ""
 let ssid = ""
 // 無線グループはこちらで設定
-let radio_group = 101
+let radio_group = 37
 // 速度は57600
 serial.redirect(
 SerialPin.P0,
@@ -52,6 +52,17 @@ radio.setFrequencyBand(radio_group % 84)
 // 受信機なので0とする
 radio.setTransmitPower(0)
 // TelloのSSIDを入れる
-ssid = "TELLO-" + "9A863A"
+ssid = "TELLO-" + "9BF7EB"
 pass = ""
 basic.showString(ssid.substr(10, 2))
+// 自動接続
+basic.showIcon(IconNames.Heart)
+serial.writeLine("ss=" + ssid)
+basic.pause(1000)
+serial.writeLine("pass=" + pass)
+basic.pause(1500)
+serial.writeLine("ss=" + ssid)
+basic.pause(1000)
+serial.writeLine("pass=" + pass)
+basic.pause(1000)
+basic.showIcon(IconNames.Yes)
